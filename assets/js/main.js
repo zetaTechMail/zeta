6ea37d8daa091131be6dee1e371031b6890606ae
iesoft <raheem.abdul@meetingfull.com>
2023-01-01 23:10:00 +0530

    Fix analytics script

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,11 +195,17 @@ function loadScriptAsync(scriptSrc, callback) {
 
 /* This is the part where you call the above defined function and "call back" your code which gets executed after the script has loaded */
 loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-RJFN27WG7P', function(){
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  var script = document.createElement('script');
+  script.textContent = 
+    `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-RJFN27WG7P');`
+  document.head.appendChild(script);
 
-  gtag('config', 'G-RJFN27WG7P');
+
 })
 
 
