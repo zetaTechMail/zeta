dac0e5860a8422e5dc9ef7f128b6fa5cb2a6ea7e
iesoft <raheem.abdul@meetingfull.com>
2023-01-01 21:03:58 +0530

    Google Analytics integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,3 +180,27 @@
   });
 
 })()
+
+
+/*This function will load script and call the callback once the script has loaded*/
+function loadScriptAsync(scriptSrc, callback) {
+  if (typeof callback !== 'function') {
+      throw new Error('Not a valid callback for async script load');
+  }
+  var script = document.createElement('script');
+  script.onload = callback;
+  script.src = scriptSrc;
+  document.head.appendChild(script);
+}
+
+/* This is the part where you call the above defined function and "call back" your code which gets executed after the script has loaded */
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-RJFN27WG7P', function(){
+  alert('Yo')
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RJFN27WG7P');
+})
+
+
